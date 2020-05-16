6b86fe3cc1bbef12441afa981fd54e1e19998b44
SigurdJanson <emailjs@web.de>
2020-05-16 12:28:25 +0200

    Fighting Tab basically works

--- src/DSADice/server.R
+++ src/DSADice/server.R
@@ -11,13 +11,55 @@ library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # LastThrow <- "NULL"
+    # 
+    # output$distPlot <- renderPlot({
+    #     # generate bins based on input$bins from ui.R
+    #     x    <- faithful[, 2]
+    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #     # draw the histogram with the specified number of bins
+    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # })
+    # 
+    # output$TraitDie <- renderText(
+    #     paste("Here it comes:", LastThrow)
+    # )
+    # 
+    # output$Thrown <- renderText(
+    #    LastThrow
+    # )
+    
+    # observeEvent(input$ThrowDice, {
+    #     #cat("Showing", input$x, "rows\n")
+    #     LastThrow <- sample.int(20, 3)
+    #     cat(LastThrow, "\n")
+    # })
+    LastThrow <- eventReactive(input$ThrowDice, {
+        sample.int(20, 3)
     })
-
+    
+    # FIGHTING TAB --------------------
+    FightVal <- reactiveValues(AT = NA, PA = NA)
+    
+    observeEvent(input$doAttackThrow, {
+        FightVal$AT <- sample.int(20, 1)
+        FightVal$PA <- NA
+    })
+    
+    observeEvent(input$doParryThrow, {
+        FightVal$PA <- sample.int(20, 1)
+        FightVal$AT <- NA
+    })  
+    
+    output$CombatAction <- renderPrint({
+        if (!is.na(FightVal$AT)) {
+            cat("Attack\n")
+            Result <- paste(FightVal$AT, ifelse(FightVal$AT <= input$ATValue, "Success", "Failure"))
+        } else if (!is.na(FightVal$PA)) {
+            cat("Parry\n")
+            Result <- paste(FightVal$PA, ifelse(FightVal$PA <= input$PAValue, "Success", "Failure"))
+        } else Result = ""
+        
+        cat(Result)
+    })
 })
--- src/DSADice/ui.R
+++ src/DSADice/ui.R
@@ -7,27 +7,58 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Dark Eye - Fate Explorer"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+# library(shiny)
+# 
+# # Define UI for application that draws a histogram
+# shinyUI(fluidPage(
+# 
+#     # Application title
+#     #,
+# 
+#     # Sidebar with a slider input for number of bins
+#     
+#     ui <- 
+        navbarPage("Dark Eye - Fate Explorer", 
+                     position = c("static-top"), inverse = TRUE, collapsible = TRUE,
+                     tabPanel("Decide Your Fate",
+                              sidebarLayout(
+                                  sidebarPanel(
+                                      radioButtons("RollType", "Type of Roll",
+                                                   c("Scatter"="p", "Line"="l")
+                                      ),
+                                      actionButton("ThrowDice", "Now!")
+                                  ),
+                                  mainPanel({
+                                      renderText(Thrown)
+                                  })
+                              )),
+                   tabPanel("Fight",
+                            sidebarLayout(
+                                sidebarPanel(
+                                    sliderInput("ATValue", "AT", min = 1, max = 20, value = 11),
+                                    sliderInput("PAValue", "PA", min = 1, max = 20, value = 4),
+                                    numericInput("Damage", "Damage W6+", value = 1),
+                                    actionButton("doAttackThrow", "Attack!", icon = icon("skull"), width = "100%"),
+                                    actionButton("doParryThrow", "Parry!", icon = icon("shield-alt"), width = "100%"),
+                                ),
+                                mainPanel(
+                                    h3(textOutput("ATValue")),
+                                    helpText("Go for it, buster!"),
+                                    #textOutput("AttackThrow"),
+                                    textOutput("CombatAction")
+                                )
+                            )),
+                   
+                   tabPanel("Explore", {}),
+                   
+                   tabPanel("Setup", {})
+    )
+    # sidebarLayout(
+    #     sidebarPanel(
+    #         
+    #     ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
-    )
-))
+       
+    # )
+#))
