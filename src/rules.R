862e0c63e989f6e6790aaa18f1a4f057c3dafa91
SigurdJanson <emailjs@web.de>
2020-05-22 15:05:08 +0200

    Moved files into src directory so that it runs on shinyapps.io

--- src/rules.R
+++ src/rules.R
@@ -9,7 +9,7 @@ require(jsonlite)
 
 GetAbilities <- function() {
   if (is.null(.Attribs)) {
-    .Attribs <<- read_json("../data/attributes_en.json", simplifyVector = TRUE)
+    .Attribs <<- read_json("./data/attributes_en.json", simplifyVector = TRUE)
   }
   return(.Attribs)
 }
@@ -17,7 +17,7 @@ GetAbilities <- function() {
 
 GetCombatTechniques <- function() {
   if (is.null(.ComTecs)) {
-    .ComTecs <<- read_json("../data/combattechs_en.json", simplifyVector = FALSE, flatten = TRUE)
+    .ComTecs <<- read_json("./data/combattechs_en.json", simplifyVector = FALSE, flatten = TRUE)
     .ComTecs <<- as.data.frame(.ComTecs, stringsAsFactors = FALSE)
   }
   return(.ComTecs)
@@ -28,7 +28,7 @@ GetWeapons <- function(Which = "All", Type = "Melee") {
   #browser()
   if (Type == "Melee") {
     if (is.null(.Melee)) {
-      .Melee <<- read_json("../data/weapon-list.json", simplifyVector = TRUE)
+      .Melee <<- read_json("./data/weapon-list.json", simplifyVector = TRUE)
     }
     if (Which == "All")
       return(.Melee)
